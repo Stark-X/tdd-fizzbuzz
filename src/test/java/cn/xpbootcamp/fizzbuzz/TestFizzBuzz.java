9393be44628ce7b8a9b6a780902b3ccd27c288f7
Jiezhi.Xiao <jzxiao@thoughtworks.com>
2020-05-09 22:03:07 +0800

    chore: tests naming changing

--- src/test/java/cn/xpbootcamp/fizzbuzz/TestFizzBuzz.java
+++ src/test/java/cn/xpbootcamp/fizzbuzz/TestFizzBuzz.java
@@ -20,24 +20,24 @@ public class TestFizzBuzz {
     }
 
     @Test
-    public void should_return_buzz_when_number_is_divisible_by_5_and_not_divisible_by_7() {
+    public void should_return_buzz_when_number_is_divisible_by_5_and_not_divisible_by_7_and_not_contains_3_and_5() {
         assertThat(new FizzBuzz(5).reply(), equalTo("Buzz"));
         assertThat(new FizzBuzz(10).reply(), equalTo("Buzz"));
     }
 
     @Test
-    public void should_return_whizz_when_number_is_divisible_by_7_and_not_contains_5() {
+    public void should_return_whizz_when_number_is_divisible_by_7_and_not_contains_3_and_5() {
         assertThat(new FizzBuzz(7).reply(), equalTo("Whizz"));
         assertThat(new FizzBuzz(14).reply(), equalTo("Whizz"));
     }
 
     @Test
-    public void should_return_fizzbuzz_when_number_is_divisible_by_3_and_5_and_not_contains_5() {
+    public void should_return_fizzbuzz_when_number_is_divisible_by_3_and_5_and_not_contains_3_and_5() {
         assertThat(new FizzBuzz(60).reply(), equalTo("FizzBuzz"));
     }
 
     @Test
-    public void should_return_fizzwhizz_when_number_is_divisible_by_3_and_7() {
+    public void should_return_fizzwhizz_when_number_is_divisible_by_3_and_7_and_not_contains_3_and_5() {
         assertThat(new FizzBuzz(21).reply(), equalTo("FizzWhizz"));
     }
 
@@ -53,7 +53,7 @@ public class TestFizzBuzz {
     }
 
     @Test
-    public void should_return_whizz_when_number_is_divisible_by_7_and_is_contains_5() {
+    public void should_return_whizz_when_number_is_divisible_by_7_and_is_contains_5_and_not_contains_3() {
         assertThat(new FizzBuzz(56).reply(), equalTo("Whizz"));
         assertThat(new FizzBuzz(105).reply(), equalTo("Whizz"));
     }
